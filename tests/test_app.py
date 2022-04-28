8a1cffcd604e45e3f90ddafc0985f3ace77b8a07
Christopher Hoffmann <the.chris@zalando.de>
2022-04-28 14:22:06 +0200

    restructure and test

---
+++ tests/test_app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Christopher Hoffmann'
+__contact__ = 'christopher.hoffmann@zalando.de'
+__license__ = 'MIT'
+__copyright__ = '(c) by Zalando SE'
+__version__ = '0.1.0'
+
+import os
+import pytest
+
+
+def test_something():
+    assert True
+
+
+if __name__ == '__main__':
+    pytest.main()
