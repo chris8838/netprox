1dcd999b0c3b02043d3767ff6cbd935a18e1e6c6
Christopher Hoffmann <the.chris@zalando.de>
2022-04-28 14:27:41 +0200

    removed import of os

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'Christopher Hoffmann'
-__contact__ = 'christopher.hoffmann@zalando.de'
-__license__ = 'MIT'
-__copyright__ = '(c) by Zalando SE'
-__version__ = '0.1.0'
+__author__ = "Christopher Hoffmann"
+__contact__ = "christopher.hoffmann@zalando.de"
+__license__ = "MIT"
+__copyright__ = "(c) by Zalando SE"
+__version__ = "0.1.0"
 
-import os
 import pytest
 
 
@@ -15,5 +14,5 @@ def test_something():
     assert True
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pytest.main()
